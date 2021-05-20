4e433c8d62780befebf51db8affcc7c4b51ca4b9
Franck Chauvel <fchauvel@Francks-MacBook-Air.local>
2021-05-20 07:55:32 +0200

    feat: Handle invalid inputs from the CLI

--- rasp/cli.py
+++ rasp/cli.py
@@ -19,9 +19,43 @@ from rasp.debug.view import DebugView
 from rasp.executable import Loader
 from rasp.machine import RASP, Profiler
 
+from pyparsing import ParseException
 from pathlib import Path
 
 from sys import argv
+
+
+
+class Presenter:
+
+    def syntax_error(self, error):
+        print(f"Syntax Error.")
+
+    def source_code_loaded_from(self, source):
+        print(f"Assembly code loaded from '{source}'.")
+
+    def missing_source_code(self):
+        print("Warning: Unable to locate the assembly source code.")
+        print(" - Assembly symbols and line number will not be available.")
+
+    def executable_created(self, executable_file):
+        print(f"Machine code written in '{executable_file}'.")
+
+    def executable_not_found(self, executable_file):
+        print(f"Error: Could not open '{executable_file}'")
+
+    def execution_failed(self, executable_file):
+        print(f"Error: Could not read '{executable_file}'")
+        print(f" - Use 'rasp assemble source.asm' to get an RASP executable file.")
+
+    def source_not_found(self, source_file):
+        print(f"Error: Could not open assembly file '{source_file}'")
+
+    def version(self):
+        print(f"{About.NAME} {About.VERSION} -- {About.DESCRIPTION}")
+        print(f"{About.COPYRIGHT}")
+        print(f"{About.LICENSE} license")
+
 
 
 class Controller:
@@ -32,30 +66,62 @@ class Controller:
     VERSION = 4
 
     def __init__(self):
+        self._present = Presenter()
         self._assembler = Assembler()
         self._load = Loader()
         self._assembly = AssemblyParser
 
-    def assemble(self, assembly_file):
-        program = self._assembly.read_file(assembly_file)
 
-        executable = self._assembler.assemble(program)
+    def assemble(self, assembly_file, include_debug, output_file):
+        try:
+            program = self._assembly.read_file(assembly_file)
 
-        self._load.save_as(executable,
-                           Path(assembly_file).with_suffix(".rx"))
+        except ParseException as error:
+            self._present.syntax_error(error)
+
+        except FileNotFoundError as error:
+            self._present.source_not_found(assembly_file)
+
+        try:
+            executable = self._assembler.assemble(program, include_debug)
+            output = Path(assembly_file).with_suffix(".rx")
+            if output_file:
+                output = Path(output_file)
+            self._load.save_as(executable, output)
+            self._present.executable_created(str(output))
+
+        except FileNotFoundError as error:
+            self._present.executable_not_found(str(output))
 
 
-    def debug(self, executable_file):
-        assembly_file = Path(executable_file).with_suffix(".asm")
-        with open(assembly_file, "r") as assembly:
-            assembly_code = assembly.read()
 
-        view = DebugView()
-        machine = RASP(input_device=view, output_device=view)
-        debug_infos = self._load.from_file(machine.memory, executable_file)
-        debugger = Debugger(machine, view, debug_infos, assembly_code)
-        session = DebugController(debugger, view)
-        session.start()
+    def debug(self, executable_file, source_file=None):
+        source_code = self._load_source_code(executable_file, source_file)
+        try:
+            view = DebugView()
+            machine = RASP(input_device=view, output_device=view)
+            debug_infos = self._load.from_file(machine.memory, executable_file)
+            debugger = Debugger(machine, view, debug_infos, source_code)
+            session = DebugController(debugger, view)
+            session.start()
+
+        except FileNotFoundError as error:
+            self._present.executable_not_found(executable_file)
+
+
+    def _load_source_code(self, executable_file, source_file):
+        source = Path(executable_file).with_suffix(".asm")
+        if source_file:
+            source = Path(source_file)
+
+        try:
+            assembly_file = source
+            with open(assembly_file, "r") as assembly:
+                self._present.source_code_loaded_from(str(source))
+                return  assembly.read()
+
+        except FileNotFoundError as error:
+            self._present.missing_source_code()
 
 
     def execute(self, executable_file, use_profiler=False):
@@ -64,20 +130,27 @@ class Controller:
             profiler = Profiler()
             machine.cpu.attach(profiler)
             machine.memory.attach(profiler)
-        with open(executable_file, "r") as code:
-            self._load.from_stream(machine.memory, code)
-            machine.run()
 
-        if use_profiler:
-            data_file = Path(executable_file).with_suffix(".perf")
-            profiler.save_results_as(data_file)
+        try:
+            with open(executable_file, "r") as code:
+                self._load.from_stream(machine.memory, code)
+                machine.run()
+                if use_profiler:
+                    data_file = Path(executable_file).with_suffix(".perf")
+                    profiler.save_results_as(data_file)
+                return 0
 
+        except FileNotFoundError as error:
+            self._present.executable_not_found(executable_file)
+            logger.error(error)
+
+        except Exception as error:
+            self._present.execution_failed(executable_file)
+            logger.error(error)
 
 
     def version(self):
-        print(f"{About.NAME} {About.VERSION} -- {About.DESCRIPTION}")
-        print(f"{About.COPYRIGHT}")
-        print(f"{About.LICENSE} license")
+        self._present.version()
 
 
 
@@ -94,6 +167,9 @@ def parse_arguments(command_line):
     assembler.add_argument("--debug", "-d",
                            help="Inline debugging information into the executable",
                            action="store_true")
+    assembler.add_argument("--output", "-o",
+                           metavar="EXE_FILE",
+                           help="Name of the executable file to generate")
     assembler.add_argument("assembly_file",
                            metavar="FILE",
                            help="The RASP assembly file to compile to machine code")
@@ -101,7 +177,9 @@ def parse_arguments(command_line):
 
     debugger = subparsers.add_parser("debug",
                                      help='starts the interactive debugger')
-
+    debugger.add_argument("--asm-source", "-s",
+                          metavar="ASM_FILE",
+                          help="Assembly source code associated with the executable")
     debugger.add_argument("executable_file",
                            metavar="FILE",
                            help="The RASP executable file to compile to debug")
@@ -133,14 +211,17 @@ def main():
     arguments = parse_arguments(argv[1:])
 
     if arguments.command == Controller.ASSEMBLE:
-        rasp.assemble(arguments.assembly_file)
+        rasp.assemble(arguments.assembly_file,
+                      arguments.debug,
+                      arguments.output)
 
     elif arguments.command == Controller.EXECUTE:
         rasp.execute(arguments.executable_file,
                      arguments.use_profiler)
 
     elif arguments.command == Controller.DEBUG:
-        rasp.debug(arguments.executable_file)
+        rasp.debug(arguments.executable_file,
+                   arguments.asm_source)
 
     elif arguments.command == Controller.VERSION:
         rasp.version()
